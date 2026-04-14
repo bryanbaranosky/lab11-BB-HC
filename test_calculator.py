465751b6f6ff83849f34ce49cec296132c9846e8
Bryan Baranosky <bryanbaranosky@gmail.com>
2026-04-13 23:30:32 -0400

    final fix

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
-#https://github.com/bryanbaranosky/lab11-BB-HC.git
-#Partner 1: Hunter Coffman
-#Partner 2: Bryan Baranosky
+# https://github.com/bryanbaranosky/lab11-BB-HC.git
+# Partner 1: Hunter Coffman
+# Partner 2: Bryan Baranosky
 
 from calculator import *
 import unittest
@@ -20,7 +20,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            divide(0, 5)
+            div(0, 5)
 
     def test_logarithm(self):
         self.assertAlmostEqual(logarithm(2, 8), 3)
@@ -33,14 +33,14 @@ class TestCalculator(unittest.TestCase):
 
     # -------- Partner 1 --------
     def test_multiply(self):
-        self.assertEqual(multiply(2, 3), 6)
-        self.assertEqual(multiply(0, 5), 0)
-        self.assertEqual(multiply(-2, 3), -6)
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(0, 5), 0)
+        self.assertEqual(mul(-2, 3), -6)
 
     def test_divide(self):
-        self.assertEqual(divide(2, 10), 5)
-        self.assertEqual(divide(5, 0), 0)
-        self.assertEqual(divide(4, 8), 2)
+        self.assertEqual(div(2, 10), 5)
+        self.assertEqual(div(5, 0), 0)
+        self.assertEqual(div(4, 8), 2)
 
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
