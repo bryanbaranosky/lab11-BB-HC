93ffec359aeb426380f260425cd1073add691a82
Bryan Baranosky <bryanbaranosky@gmail.com>
2026-04-13 22:49:50 -0400

    p2 changes

--- calculator.py
+++ calculator.py
@@ -30,3 +30,25 @@ def logarithm(a, b):
 
 def exponent(a, b):
     return a ** b
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
